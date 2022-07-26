ca5de1278299c84602da611750ca0dd8abff2f4b
Stephanie Chan <103970034+eina-chan@users.noreply.github.com>
2022-07-26 14:27:56 +1000

    clean up

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -24,12 +24,10 @@ const MessagesDisplay = React.forwardRef(function MessagesDisplay(
   // 🐨 call useImperativeHandle here with your ref and a callback function
   // that returns an object with scrollToTop and scrollToBottom
 
-  React.useImperativeHandle(ref, () => {
-    return {
-      scrollToTop: () => scrollToTop(),
-      scrollToBottom: () => scrollToBottom(),
-    }
-  })
+  React.useImperativeHandle(ref, () => ({
+    scrollToTop,
+    scrollToBottom,
+  }))
 
   return (
     <div ref={containerRef} role="log">
